5e01402798edba07eddcf3ee2d2e197d0a225f44
Coko33 <nicolasplacido@gmail.com>
2023-05-31 21:59:54 -0300

    Agregar archivos de proyecto.

---
+++ Migrations/20230601001032_PNT1_Grupo6.Context.Estudiante-Apellido.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PNT1_Grupo6.Migrations
+{
+    public partial class PNT1_Grupo6ContextEstudianteApellido : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apellido",
+                table: "Estudiantes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellido",
+                table: "Estudiantes");
+        }
+    }
+}
---
+++ Models/Deporte.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace PNT1_Grupo6.Models
+{
+	public enum Deporte
+	{
+		Natacion,
+		Futbol,
+		Voley,
+		Basquet,
+		Truco,
+		Ajedrez
+	}
+}
