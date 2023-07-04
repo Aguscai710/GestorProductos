b20601c18270bd6049fdb1c2e5a46736df85f33d
Coko33 <nicolasplacido@gmail.com>
2023-07-04 20:14:46 -0300

    estilos para las tablas

---
+++ Migrations/20230702212710_Actualizaion_BDNico2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PNT1_Grupo6.Migrations
+{
+    public partial class Actualizaion_BDNico2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20230702223743_Estilos_Nico1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PNT1_Grupo6.Migrations
+{
+    public partial class Estilos_Nico1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
