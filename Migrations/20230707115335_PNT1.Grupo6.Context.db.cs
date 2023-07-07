14fc176b03630eee0fe7e48a477ca6aab4c6a4e5
Aguscai710 <agustinscianfortewk@gmail.com>
2023-07-07 09:15:03 -0300

    RETOQUE DE ESTILOS

---
+++ Migrations/20230707115335_PNT1.Grupo6.Context.db.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PNT1_Grupo6.Migrations
+{
+    public partial class PNT1Grupo6Contextdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
